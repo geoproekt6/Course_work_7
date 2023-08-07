937b515684283cc1e755d6de639579a1ead2ac9d
Blendi <dronramone@mail.ru>
2023-08-08 00:26:41 +0400

    Переопределена модель пользователя Создана кастомная команда создания суперюзера

--- users/models.py
+++ users/models.py
@@ -1,8 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True,
+}
 
 
 class User(AbstractUser):
-    pass
+
+    username = None
+
+    email = models.EmailField(verbose_name='почта', unique=True)
+    tg_login = models.CharField(max_length=127, **NULLABLE, verbose_name='Логин в телеграме')
+    chat_id = models.CharField(max_length=127, **NULLABLE, verbose_name='ID чата в телеграме')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}, {self.tg_login}, {self.chat_id}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
+
+
