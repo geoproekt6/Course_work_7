06f5b3975ce690507fa2efd1af15bd57f690b3e9
Blendi <dronramone@mail.ru>
2023-08-07 23:47:11 +0400

    Create apps User & habits

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'users',
+    'habits',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
