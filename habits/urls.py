ae6acf6e07d2f14112187e23d83128c7d7c10e54
Blendi <dronramone@mail.ru>
2023-08-08 13:54:50 +0400

    Заготовка CRUD для  модели Habbits

--- habits/urls.py
+++ habits/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
+from habits.views import HabbitsCreateAPIView, HabbitsListAPIView, HabbitsDetailAPIView, HabbitsUpdatelAPIView, \
+    HabbitsDeleteAPIView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-
+    path('create/', HabbitsCreateAPIView.as_view(), name='habbits_create'),
+    path('', HabbitsListAPIView.as_view(), name='habbits_list'),
+    path('<int:pk>/', HabbitsDetailAPIView.as_view(), name='habbits_detail'),
+    path('update/<int:pk>/', HabbitsUpdatelAPIView.as_view(), name='habbits_update'),
+    path('delete/<int:pk>/', HabbitsDeleteAPIView.as_view(), name='habbits_delete'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+from habits.models import Habbits
+
 
 # Create your views here.
+
+class HabbitsCreateAPIView(CreateAPIView):
+    """"""
+    serializer_class = ...
+
+    class Meta:
+        model = Habbits
+        fields = '__all__'
+
+
+class HabbitsListAPIView(ListAPIView):
+    """"""
+    queryset = Habbits.objects.all()
+    serializer_class = ...
+
+    class Meta:
+        model = Habbits
+        fields = '__all__'
+
+
+class HabbitsDetailAPIView(RetrieveAPIView):
+    """"""
+    queryset = Habbits.objects.all()
+    serializer_class = ...
+
+    class Meta:
+        model = Habbits
+        fields = '__all__'
+
+
+class HabbitsUpdatelAPIView(UpdateAPIView):
+    """"""
+    queryset = Habbits.objects.all()
+    serializer_class = ...
+
+    class Meta:
+        model = Habbits
+        fields = '__all__'
+
+
+class HabbitsDeleteAPIView(DestroyAPIView):
+    """"""
+    queryset = Habbits.objects.all()
+
+    class Meta:
+        model = Habbits
+        fields = '__all__'
